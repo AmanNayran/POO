2860b5a3a10e9ad04f77601488d59f518de9f345
Aman Nayran <62403480+AmanNayran@users.noreply.github.com>
2022-08-10 17:12:48 -0300

    Update q09.js

--- poo_exercicio_00_2022/q09.js
+++ poo_exercicio_00_2022/q09.js
@@ -1,1 +1,10 @@
-//Em andamento
+var v = 1000
+var t = 0.01
+var jcomposto = []
+
+for (var i = 1; i <= 12; i++) {
+    var montante = v * Math.pow((1 + t), i)
+    var adicionar = jcomposto.push(montante.toFixed(2))
+}
+
+console.log(jcomposto)
