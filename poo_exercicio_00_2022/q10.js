89f01c56d6d1ba76b94bfbe7a4c74590a3465cc2
Aman Nayran <62403480+AmanNayran@users.noreply.github.com>
2022-08-11 09:51:34 -0300

    Update q10.js

--- poo_exercicio_00_2022/q10.js
+++ poo_exercicio_00_2022/q10.js
@@ -1,1 +1,15 @@
-//Em andamento
+var milissegundos = 90061001
+
+const dias = parseInt(milissegundos / 86400000)
+milissegundos = milissegundos % 86400000
+
+const horas = parseInt(milissegundos / 3600000)
+milissegundos = milissegundos % 3600000
+
+const minutos = parseInt(milissegundos / 60000)
+milissegundos = milissegundos % 60000
+
+const segundos = parseInt(milissegundos / 1000)
+milissegundos = milissegundos % 1000
+
+console.log(`${dias}:${horas}:${minutos}:${segundos}:${milissegundos}`)
