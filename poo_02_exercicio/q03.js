997eedd4c4c08f8f84e4211ad63f131a9858344c
Aman Nayran <62403480+AmanNayran@users.noreply.github.com>
2022-09-18 23:16:59 -0300

    Exercicio 02

---
+++ poo_02_exercicio/q03.js
@@ -0,0 +1,6 @@
+// Pesquise um exemplo na internet em que a tipagem dinâmica pode ser problemático.
+
+const num = "1"
+const numb = 2  // é convertido para string para fazer a concatenação com a var "num"
+
+console.log(num + numb) // 12
---
+++ poo_02_exercicio/q07.ts
@@ -0,0 +1,14 @@
+// Reescreva o exemplo abaixo, mantendo a quebra de linhas usando template
+// strings e os valores Ely, 120.56 e TypeScript venham de variáveis declaradas
+// separadamente e “interpoladas” na string:
+
+const nome: string = "Ely"
+const pay: number = 120.56
+const language: string = "TypeScript"
+
+console.log(`
+${nome}    
+My payment time is ${pay}
+and
+my preffered language is ${language}
+`)
